97189435cbabc59317e15b213a0fbcc4ff80a78c
Kristof Liliom <kristof.liliom@mattakis.com>
2016-04-04 10:52:55 +0200

    0f5605e080510ff774bc2dd90336844295a56514

--- TestSDK.framework/Headers/Test.h
+++ TestSDK.framework/Headers/Test.h
@@ -1,9 +1,13 @@
 //
-//  TestSDK.h
+//  Test.h
 //  TestSDK
 //
 //  Created by kovacsi on 31/03/16.
 //  Copyright © 2016 Mattakis Ltd. All rights reserved.
 //
 
-#import <TestSDK/Test.h>
+@interface Test : NSObject
+
++ (void)test;
+
+@end
